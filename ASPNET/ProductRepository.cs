f6b8ead21e9b4c2888023144486c711469cd0876
jason-parson <jpsells4u@gmail.com>
2020-07-28 14:51:46 -0500

    updated program

---
+++ ASPNET/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using ASPNET.Models;
+using Dapper;
+
+namespace ASPNET
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts() 
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+            
+
+    }
+}
